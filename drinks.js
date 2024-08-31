74db7dba0a86b5459e31b7e0a2332c380dbd0343
unknown <sr0589071@gmail.com>
2024-08-31 06:24:14 +0600

    first commmit

---
+++ drinks.js
@@ -0,0 +1,9 @@
+let burgerPrice = 700;
+  if(burgerPrice > 500){
+    console.log("Coke Free");
+  }
+  else{
+    console.log("coke:30tk");
+  }
+
+  // You can get coke free cause your burger price more than 500tk.
