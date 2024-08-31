8749d1464a224d50525c40a3a69d7b9eba8198f9
unknown <sr0589071@gmail.com>
2024-08-31 07:59:30 +0600

    create nestedfriend.js file

---
+++ nestedfriend.js
@@ -0,0 +1,24 @@
+let myMark = 75;
+let friendMark = 82;
+
+
+ if (myMark > 80){
+     console.log("inside your friend score");
+ }
+//  else  if(friendMark > 80){
+//      console.log("go to a lunch");
+//  }
+
+// else if(friendMark < 80 || friendMark >=60){
+//     console.log("good luck next time");
+// }
+
+// else if(friendMark <60 || friendMark >= 40){
+//     console.log("keep your friends message unseen");
+// }
+else if(friendMark < 40){
+    console.log("block your friend");
+}
+// else{
+//     console.log("go to home and sleep and act sad");
+// }
